0f2ef9661fb6cd34d03805ba9ab8a3369d91da72
Renan Melo <renan_gian@hotmail.com>
2019-06-26 20:53:40 -0300

    adicionando middlewares

--- index.js
+++ index.js
@@ -4,14 +4,43 @@ const users = ["Renan", "Moscinga", "Mocingona"];
 
 const server = express();
 
+//middleware global, ele é um interceptador, se n usar o next, ele n continua
+
+server.use((req, res, next) => {
+  console.log(`Metodo ${req.method}; ULR : ${req.url}`);
+
+  return next();
+});
+
+function checkUserExists(req, res, next) {
+  if (!req.body.name)
+    return res.status(400).json({ error: "Espera o param nome" });
+
+  return next();
+}
+
+function existsUserInArray(req, res, next) {
+  const user = users[req.params.id];
+
+  if (!user) return res.status(400).json({ erro: "User n existe" });
+
+  // fazendo isso, agora todas as rotas que tiverem acesso a
+  // checkUserExists, agora tbm terá acesso a user, pois foi definido
+  // logo abaixo.
+  req.user = user;
+  return next();
+}
+
+// end mdw
+
 // modulo que entende json, por parametro o epress não conhece json nas req
 server.use(express.json());
 
-server.get("/users/:index", (req, res) => {
-  const id = req.params.index;
-
+server.get("/users/:id", existsUserInArray, (req, res) => {
+  // const { id } = req.params;
+  // esse user vem do existsUserInArray, pois ele da acesso
   return res.json({
-    message: `ola ${users[id]}`
+    message: `ola ${req.user}`
   });
 });
 
@@ -19,7 +48,7 @@ server.get("/users", (req, res) => {
   return res.json(users);
 });
 
-server.post("/users", (req, res) => {
+server.post("/users", checkUserExists, (req, res) => {
   const { name } = req.body;
 
   users.push(name);
@@ -27,7 +56,7 @@ server.post("/users", (req, res) => {
   return res.json(users);
 });
 
-server.put("/users/:id", (req, res) => {
+server.put("/users/:id", checkUserExists, (req, res) => {
   const { id } = req.params;
 
   const { user } = req.body;
